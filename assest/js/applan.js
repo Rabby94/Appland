d7cd2023cb70da89502d4b643f0d0d1a17e29048
unknown <Rabby94>
2022-04-19 10:36:41 +0600

    01

--- assest/js/applan.js
+++ assest/js/applan.js
@@ -2,9 +2,7 @@
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+  /*-------------------------Easy selector helper function-------------------------*/
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -13,10 +11,9 @@
       return document.querySelector(el)
     }
   }
+  /*-------------------------Easy selector helper function End-------------------------*/
 
-  /**
-   * Easy event listener function
-   */
+  /* ------------------------Easy event listener function------------------------------*/
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -27,17 +24,15 @@
       }
     }
   }
+  /* ------------------------Easy event listener function End------------------------------*/
 
-  /**
-   * Easy on scroll event listener 
-   */
+  /* -----------------------Easy on scroll event listener-------------------------------- */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
+  /* -----------------------Easy on scroll event listener End-------------------------------- */
 
-  /**
-   * Navbar links active state on scroll
-   */
+  /*------------------------ Navbar links active------------------------------*/
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -54,10 +49,9 @@
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
+  /*------------------------ Navbar links active End------------------------------*/
 
-  /**
-   * Scrolls to an element with header offset
-   */
+  /* ----------------------Scrolls to an element with header offset----------------*/
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -67,10 +61,9 @@
       behavior: 'smooth'
     })
   }
+  /* --------------------Scrolls to an element with header offset End----------------*/
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
+  /*------------------------------------Toggle-------------------------------------*/
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
@@ -83,10 +76,9 @@
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
   }
+  /*------------------------------------Toggle End-------------------------------------*/
 
-  /**
-   * Back to top button
-   */
+  /*-------------------------------------Back to top button---------------------------*/
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -99,29 +91,26 @@
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
+  /*-------------------------------Back to top button End---------------------------*/
 
-  /**
-   * Mobile nav toggle
-   */
+  /*----------------------------------Mobile nav toggle----------------------------*/
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
+  /*----------------------------------Mobile nav toggle End----------------------------*/
 
-  /**
-   * Mobile nav dropdowns activate
-   */
+  /*-------------------------Mobile nav dropdowns activate-----------------------------*/
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
       this.nextElementSibling.classList.toggle('dropdown-active')
     }
   }, true)
+  /*-------------------------Mobile nav dropdowns activate End-----------------------------*/
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+  /*-----------------------------------Scrool with ofset ----------------------*/
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -136,10 +125,9 @@
       scrollto(this.hash)
     }
   }, true)
+  /*-----------------------------------Scrool with ofset  End----------------------*/
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
+  /*-------------------------Scroll with ofset on page load---------------------------*/
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -147,10 +135,9 @@
       }
     }
   });
+  /*-------------------------Scroll with ofset on page load---------------------------*/
 
-  /**
-   * Gallery Slider
-   */
+  /*----------------------------------Gallery Slider---------------------------*/
   new Swiper('.gallery-slider', {
     speed: 400,
     loop: true,
@@ -184,17 +171,15 @@
       }
     }
   });
+  /*----------------------------------Gallery Slider  End---------------------------*/
 
-  /**
-   * Initiate gallery lightbox 
-   */
+  /*------------------------------Initiate gallery lightbox--------------------*/
   const galleryLightbox = GLightbox({
     selector: '.gallery-lightbox'
   });
+  /*------------------------------Initiate gallery lightbox End--------------------*/
 
-  /**
-   * Testimonials slider
-   */
+  /*------------------------------------Testimonials slider-------------------------*/
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -220,10 +205,9 @@
       }
     }
   });
+  /*------------------------------------Testimonials slider End-------------------------*/
 
-  /**
-   * Animation on scroll
-   */
+  /*------------------------------------Animation on scroll-------------------------------*/
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
@@ -233,4 +217,5 @@
     });
   });
 
-})()
+})() 
+ /*------------------------------------Animation on scroll-------------------------------*/
